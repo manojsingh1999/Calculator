7e76525d32f18db54e43bcc71e4a65db50e017ce
Hey Manoj singh <95011806+manojsingh1999@users.noreply.github.com>
2023-07-19 12:27:25 +0530

    Update CalculatorTest.java

--- src/test/java/com/houarizegai/calculator/CalculatorTest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/*
+     class calculator
+     */
 class CalculatorTest {
 
     private Calculator calculator;
